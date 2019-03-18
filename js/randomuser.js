98afe6f9d5ef591b16226bab10d3a253e806b2d6
steven chow <1015041018@njupt.edu.cn>
2019-03-18 19:56:00 +0800

    Update randomuser.js

--- js/randomuser.js
+++ js/randomuser.js
@@ -6,19 +6,19 @@ function append(parent, el) {
       return parent.appendChild(el);
 }
 
-const ul = document.getElementById('randomuser');
-const url = 'https://randomuser.me/api/?results=3';
+const ul = document.getElementById('randomuser'); // get the list where we place our results
+const url = 'https://randomuser.me/api/?results=3'; // get results about 3 random  generated users
 fetch(url)
-.then((resp) => resp.json())
+.then((resp) => resp.json()) // transform the data into json
 .then(function(data) {
       let authors = data.results;
       return authors.map(function(author) {
             let li = createNode('li'), img = createNode('img'), span = createNode('span');
             img.src = author.picture.medium;
             span.innerHTML = `${author.name.first} ${author.name.last}`;
-            append(li, img);
-            append(li, span);
-            append(ul, li);
+            append(li, img); // list item
+            append(li, span); // list item
+            append(ul, li); // add the list item into the unordered list
       })
 })
 .catch(function(error) {
