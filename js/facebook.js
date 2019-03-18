e96e899cce0835b6c8c82581d0a5e2a860c8806d
steven chow <1015041018@njupt.edu.cn>
2019-03-18 17:00:58 +0800

    Update facebook.js

--- js/facebook.js
+++ js/facebook.js
@@ -1,9 +1,8 @@
-
 FB.api(
     '/me/posts',
     'GET',
     {"fields":"message,full_picture,created_time"},
     function(response) {
-        // Insert your code here
+        alert(response);
     }
 );
