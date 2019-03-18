9cca40ea84e0bcfebe7bac4932c5796933c9f3bc
steven chow <1015041018@njupt.edu.cn>
2019-03-18 18:26:27 +0800

    Update eSathi.js

--- js/eSathi.js
+++ js/eSathi.js
@@ -1,11 +1,3 @@
-function createNode(element) {
-      return document.createElement(element);
-}
-
-function append(parent, el) {
-      return parent.appendChild(el);
-}
-
 const ul_e = document.getElementById('esathi');
 const url_e = 'https://www.e-sathi.com/api.php?get=pages&query=unelmamovie';
 fetch(url_e)
