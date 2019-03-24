c80b963ff328d7f7def5bd02017184e17c6dfddc
steven chow <1015041018@njupt.edu.cn>
2019-03-24 17:56:01 +0800

    Update facebook.js

--- js/facebook.js
+++ js/facebook.js
@@ -1,5 +1,5 @@
 FB.api(
-    "/564897400669317/feed",
+    "/564897400669317/feed?fields=permalink_url&access_token=332574250722556|VqjmBe8UKO_QCU-akXOGSzBCwOk",
     function (response) {
       if (response && !response.error) {
         alert(response);
