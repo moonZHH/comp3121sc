49eddee00b1efb5603d09407fbdf1857405bb534
steven chow <1015041018@njupt.edu.cn>
2019-03-31 17:39:14 +0800

    Delete facebook.js

--- js/facebook.js
+++
@@ -1,8 +0,0 @@
-FB.api(
-    "/564897400669317/feed?fields=permalink_url&access_token=332574250722556|VqjmBe8UKO_QCU-akXOGSzBCwOk",
-    function (response) {
-      if (response && !response.error) {
-        alert(response);
-      }
-    }
-);
